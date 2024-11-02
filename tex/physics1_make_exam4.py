d4826030a774d0a8f0ae6661725aa692fdfd6d33
davidwhogg <david.hogg@nyu.edu>
2024-11-02 11:38:36 -0400

    ready

--- tex/physics1_make_exam4.py
+++ tex/physics1_make_exam4.py
@@ -10,21 +10,39 @@ header = r"""
 
 problems = [
 r"""\begin{problem} (From Problem Set 6)
+In the static ``hanging sign'' problem, what is the tension $T_1$ in
+the upper string? Give your answer as a symbolic expression.
+\marginpar{\includegraphics[width=\marginparwidth]{../mp/hanging_sign.pdf}}
 \end{problem}
 """,
 r"""\begin{problem} (From Problem Set 7)
+We considered a typical adult holding a
+$6\,\kg$ grocery bag by its handle in the hand, with the arm bent at $90\,\deg$.
+Roughly what tension did you find in the tendon? Give your answer with clear units.
 \end{problem}
 """,
 r"""\begin{problem} (From Problem Set 6)
+In Lecture we dropped a pool ball from a height of about $1\,\m$. It
+bounced off of the cement floor. Roughly what was the impulse
+delivered to the ball from the floor? Give a numerical answer with clear units.
 \end{problem}
 """,
 r"""\begin{problem} (From Problem Set 7)
+A mass $M$ hangs from a spring of spring constant $k$ and natural length $\ell_0$ in
+a gravitational field with acceleration $g$. Because of gravity, even at equilibrium,
+the spring will be stretched to a new equilibrium length. Write an expression for this
+new equilibrium length.
 \end{problem}
 """,
 r"""\begin{problem} (From Recitation)
+What---in SI base units---are the units of the product $P\,V$ of a pressure $P$ times a volume $V$?
+Recall that the base units are $\kg$, $\m$, and $\s$.
 \end{problem}
 """,
 r"""\begin{problem} (From Lecture)
+An ice cube floats in a glass of water.
+When the ice cube melts, does the water level go up or down or stay the same?
+Explain why.
 \end{problem}
 """,
 ]
