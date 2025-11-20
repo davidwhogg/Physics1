87078e35edd9fa08fee80bef0da681a720fc9a81
davidwhogg <david.hogg@nyu.edu>
2025-11-20 06:42:05 -0500

    update to 2025

--- tex/physics1_make_exam5.py
+++ tex/physics1_make_exam5.py
@@ -1,7 +1,7 @@
 import numpy as np
 np.random.seed(17)
 nproblem = 6 # magic
-nstudent = 60 # magic
+nstudent = 70 # magic
 
 header = r"""
 \examheader{Term Exam 5}
@@ -9,43 +9,43 @@ header = r"""
 """
 
 problems = [
-r"""\begin{problem} (From Problem Set 8
-A long, thin rod of length $L$ and cross-sectional area $A$ and
-elastic (Young's) modulus $E$ and mass $M$. What combination of these variables
-predicts (by dimensional analysis) the frequency $\omega$ of oscillations of this rod?
-\end{problem}
-""",
-r"""\begin{problem} (From Problem Set 9)
-What is the correction to your weight coming from buoyancy, roughly? Express it as a fraction of the magnitude of the gravitational force.
-\end{problem}
-""",
 r"""\begin{problem} (From Problem Set 8)
-Imagine you have an unusual friend called ``mini-you'' who is identical to you but
-exactly one half your size in every dimension (half as tall, half as wide, half as deep, one-eighth your mass).
-You and mini-you both walk at natural stride rates.
-What is the ratio $T_\mathrm{you}/T_\mathrm{mini-you}$ of the period $T_\mathrm{you}$ of your natural stride
-to the period $T_\mathrm{mini-you}$ of mini-you's natural stride?
-\end{problem}
-""",
-r"""\begin{problem} (From Problem Set 9)
-Compare a point $A$ that is at the surface of the ocean
-to a point $B$ that is $10\,\m$ below the surface of the ocean.
-By how much is the pressure larger at point $B$ than at point $A$?
-That is, what is $P_B - P_A$?
-Give a numerical answer in SI units.
+If
+$$ x(t) = A\,\e^{-\frac{\gamma}{2}\,t}\,\cos (\omega\,t + \phi) ~,$$
+then what is $\dd x/\dd t$?
 \end{problem}
 """,
 r"""\begin{problem} (From Recitation)
-The Earth orbits, at a mean distance of $1\,\au$, the Sun.
-Imagine instead that the Earth orbits, at the same mean distance, a black hole that is 36 times the the mass of the Sun.
-What is the length of the year in this new situation?
+What is the mass of the Milky Way, approximately, in terms of the mass of the Sun?
+\end{problem}
+""",
+r"""\begin{problem} (From Problem Set 8)
+A very thin ladder of length $L$ and mass $M$ leans against a vertical
+wall, on a horizontal floor, making an angle of $\theta$ with respect
+to the wall.  Imagine that there is a large coefficient of friction
+$\mu$ at the floor so that the ladder is in static
+equilibrium, but assume that the wall is effectively frictionless.
+Draw a free-body diagram for the ladder, showing all
+forces acting, and where (on the ladder) that they act.
 \end{problem}
 """,
 r"""\begin{problem} (From Lecture)
-We compared two collisions, one of a rod of mass $m$ and a putty ball of mass $m$, and the other of two putty balls, both of mass $m$.
-Both collisions are inelastic, because the putty ``sticks'' or kinetic energy is lost (converted to heat maybe).
-In which collision is more kinetic energy lost, or are they both the same?
-If one is less and one is more, why?
+How is $g$ (the acceleration due to gravity at the surface of the Earth) related
+to the total mass $M$ and radius $R$ of the Earth? Give a symbolic expression using
+whatever constants you need.
+\end{problem}
+""",
+r"""\begin{problem} (From Recitation)
+If a flatbed truck is moving at speed $v_t$ in the $x$ direction relative to the ground,
+and a pipe is moving at speed $v_p$ in the $x$ direction with respect to the ground,
+and the pipe is rolling without slipping on the truck bed,
+at what angular speed $\omega$ is the pipe turning? Don't worry about sign, just give me a magnitude.
+\end{problem}
+""",
+r"""\begin{problem} (From Recitation)
+The year is roughly 360 days long.
+If the Earth were four times less massive than it is, but were still on a
+circular orbit around our Sun at its current radius, what, roughly, would be the length of the year?
 \end{problem}
 """,
 ]
